782326215115fab25da59c124e3ae3547efc9646
indigenajacobo <indigenajacobo123@gmail.com>
2026-01-17 20:19:12 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,39 +8,8 @@ export const metadata = {
   title: 'RETORNOS DE PAREJA',
   description: 'HABLANOS YA HAY SOLUCION',
 }
-
-export default function RootLayout({ children }) {
-
-  const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID
-  const TEST_CODE = process.env.NEXT_PUBLIC_TEST_CODE
-
-  return (
-    <html lang="en">
-      <head>
-        {/* Meta Pixel Code */}
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${FB_PIXEL_ID}');
-              fbq('track', 'PageView', {}, { eventID: ${TEST_CODE} });
-            `,
-          }}
-        />
-
-      </head>
-      <body className='bg-gray-800'>
-        {children}
-        {/* <Script
+const TEST_CODE = process.env.NEXT_PUBLIC_TEST_CODE
+{/* <Script
           id="fb-whatsapp-fallback"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
@@ -63,34 +32,45 @@ export default function RootLayout({ children }) {
         `,
           }}
         /> */}
+export default function RootLayout({ children }) {
+  const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
+
+  return (
+    <html lang="en">
+      <head>
         <Script
-          id="fb-pixel"
+          id="facebook-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-
-              fbq('init', '${FB_PIXEL_ID}');
-              fbq('track', 'PageView');
-            `,
+                      !function(f,b,e,v,n,t,s)
+                      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                      n.queue=[];t=b.createElement(e);t.async=!0;
+                      t.src=v;s=b.getElementsByTagName(e)[0];
+                      s.parentNode.insertBefore(t,s)}(window, document,'script',
+                      'https://connect.facebook.net/en_US/fbevents.js');
+        
+                      fbq('init', '${FB_PIXEL_ID}');
+                      fbq('track', 'PageView');
+                    `,
           }}
         />
+      </head>
+
+      <body className="bg-gray-800">
+        {children}
+
         <noscript>
           <img
             height="1"
             width="1"
-            style={{ display: 'none' }}
+            style={{ display: "none" }}
             src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
           />
         </noscript>
       </body>
     </html>
-  )
+  );
 }
